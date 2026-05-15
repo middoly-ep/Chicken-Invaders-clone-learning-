846391f11fcd992c93f0947c30b678263f6b1a84
MidDoly <middolynotclone@gmail.com>
2026-05-15 10:27:02 +0700

    feat: Keep player inside screen

--- Chicken Invaders clone/Assets/Scripts/PlayerMovement.cs
+++ Chicken Invaders clone/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float screenHorizontalLimit = 10.5f;
+    private float screenVerticalLimit = 4.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +16,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
-
+        
+        mouseWorldPosition.y = Mathf.Clamp(mouseWorldPosition.y, -screenVerticalLimit, screenVerticalLimit);
+        mouseWorldPosition.x = Mathf.Clamp(mouseWorldPosition.x, -screenHorizontalLimit, screenHorizontalLimit);
         mouseWorldPosition.z = transform.position.z;
         transform.position = mouseWorldPosition;
+        
     }
 }
